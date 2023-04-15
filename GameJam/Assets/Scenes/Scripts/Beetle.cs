62e313cdf83cc5057277e06c1e62d24896c58fbd
Boleslav Horak <boleslavh@gmail.com>
2023-04-15 22:10:24 +0200

    Perpared level building assets

--- GameJam/Assets/Scenes/Scripts/Beetle.cs
+++ GameJam/Assets/Scenes/Scripts/Beetle.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SeweralIdeas.Pooling;
 using SeweralIdeas.UnityUtils;
-
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
